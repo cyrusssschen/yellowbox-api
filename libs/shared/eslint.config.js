1f86033415ef11d4d4f882ebc1352bb30c953452
Cyrus Chen <kingwinchan@126.com>
2024-10-21 08:33:25 +1100

    Fix: updated .env.example files

--- libs/shared/eslint.config.js
+++ libs/shared/eslint.config.js
@@ -15,14 +15,5 @@ module.exports = [
     languageOptions: {
       parser: require('jsonc-eslint-parser'),
     },
-  },
-  {
-    files: ['**/*.ts'],
-    rules: {
-      "@nx/interface-name-prefix": "off",
-      "@nx/explicit-function-return-type": "off",
-      "@nx/explicit-module-boundary-types": "off",
-      "@typescript-eslint/no-explicit-any": ["off"]
-    }
   }
 ];
